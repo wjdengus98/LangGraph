a5783e9a8df77d4a8cece397374f008dea37e560
wjgusen98 <jhd6290@gmail.com>
2026-01-27 14:09:02 +0900

    feat: add hallucination and router

--- graph/graph.py
+++ graph/graph.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 from langgraph.graph import END, StateGraph
 from graph.consts import RETRIEVE,GRADE_DOCUMENTS,GENERATE,WEBSEARCH
 #from graph.nodes import generate,grade_documents,retrieve,web_search
+from graph.chains.answer_grader import answer_grader
+from graph.chains.hallucination_grader import hallucination_grader
+from graph.chains.router import RouteQuery, question_router
 from graph.state import GraphState
 from graph.nodes.retrieve import retrieve
 from graph.nodes.grade_documents import grade_documents
@@ -22,6 +25,42 @@ def decide_to_generate(state):
     else:
         print("---DECISION: GENERATE---")
         return GENERATE
+
+def grade_generation_grounded_in_documents_and_question(state: GraphState) -> str:
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+
+    if hallucination_grade := score.binary_score:
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        if answer_grade := score.binary_score:
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+    
+def route_question(state: GraphState) -> str:
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    source: RouteQuery = question_router.invoke({"question": question})
+    if source.datasource == WEBSEARCH:
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return WEBSEARCH
+    elif source.datasource == "vectorstore":
+        print("---ROUTE QUESTION TO RAG---")
+        return RETRIEVE
+
     
 workflow = StateGraph(GraphState)
 
@@ -30,14 +69,30 @@ workflow.add_node(GRADE_DOCUMENTS, grade_documents)
 workflow.add_node(GENERATE, generate)
 workflow.add_node(WEBSEARCH, web_search)
 
-workflow.set_entry_point(RETRIEVE)
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        WEBSEARCH: WEBSEARCH,
+        RETRIEVE: RETRIEVE,
+    },
+)
 workflow.add_edge(RETRIEVE, GRADE_DOCUMENTS)
 workflow.add_conditional_edges(
     GRADE_DOCUMENTS,
     decide_to_generate,
     {
         WEBSEARCH: WEBSEARCH,
-        RETRIEVE: RETRIEVE,
+        GENERATE: GENERATE,
+    },
+)
+
+workflow.add_conditional_edges(
+    GENERATE,
+    grade_generation_grounded_in_documents_and_question,
+    {
+        "not supported": GENERATE,
+        "useful": END,
+        "not useful": WEBSEARCH,
     },
 )
 workflow.add_edge(WEBSEARCH, GENERATE)
