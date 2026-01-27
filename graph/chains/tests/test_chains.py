93805e3a5fc5f24ec2597e6badc9cb77ffc9994b
wjgusen98 <jhd6290@gmail.com>
2026-01-27 14:09:21 +0900

    feat: add more test_chain

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
+from graph.chains.router import RouteQuery, question_router
 
 load_dotenv()
 
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from graph.chains.hallucination_grader import (GradeHallucinations,
+                                               hallucination_grader)
 from graph.chains.generation import generation_chain
 from ingestion import retriever
 
@@ -31,3 +34,38 @@ def test_generation_chain() -> None:
     docs = retriever.invoke(question)
     generation = generation_chain.invoke({"context": docs, "question" : question})
     print(generation)
+    
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
+
+def test_router_to_vectorstore() -> None:
+    question = "agent memory"
+
+    res: RouteQuery = question_router.invoke({"question": question})
+    assert res.datasource == "vectorstore"
+
+
+def test_router_to_websearch() -> None:
+    question = "how to make pizza"
+
+    res: RouteQuery = question_router.invoke({"question": question})
+    assert res.datasource == "websearch"
